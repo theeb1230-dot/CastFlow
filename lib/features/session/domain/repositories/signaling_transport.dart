1192209d2eea8923fdcae75cdb32f062c8c88050
Arab_Theeb <theeb1230@gmail.com>
2026-09-05 04:17:46 +0300

    fix(format): normalize signaling transport signature

--- lib/features/session/domain/repositories/signaling_transport.dart
+++ lib/features/session/domain/repositories/signaling_transport.dart
@@ -3,8 +3,5 @@ import '../entities/signaling_message.dart';
 abstract interface class SignalingTransport {
   Stream<SignalingMessage> get messages;
 
-  Future<void> send(
-    SignalingMessageType type,
-    Map<String, Object?> payload,
-  );
+  Future<void> send(SignalingMessageType type, Map<String, Object?> payload);
 }
