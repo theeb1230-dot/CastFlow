4bca932aca7e7331ebdd0a41809dde0a48aa98cc
Arab_Theeb <theeb1230@gmail.com>
2026-09-05 03:43:46 +0300

    phase 1: add lib/features/dashboard/presentation/bloc/discovery_cubit.dart

---
+++ lib/features/dashboard/presentation/bloc/discovery_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+enum DiscoveryStatus { idle, scanning }
+
+class DiscoveryCubit extends Cubit<DiscoveryStatus> {
+  DiscoveryCubit() : super(DiscoveryStatus.idle);
+
+  void toggle() => emit(
+        state == DiscoveryStatus.idle
+            ? DiscoveryStatus.scanning
+            : DiscoveryStatus.idle,
+      );
+}
